4660e22961ac1e7a95ff5fd67f183a0636d5eee4
Anderson Silva <anderson.silva@cujo.com>
2018-07-16 15:48:32 -0700

    Add. Initial commit

---
+++ api/ex1.c
@@ -0,0 +1,34 @@
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+int myadd(lua_State* L);
+
+int main()
+{
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
+
+	lua_pushcfunction(L, myadd);
+	lua_setglobal(L, "add");
+
+	luaL_dofile(L, "test.lua");
+
+	lua_getglobal(L, "x");
+
+	printf("result=%f\n", lua_tonumber(L, -1));
+
+	lua_close(L);
+
+	return 0;
+}
+
+int myadd(lua_State* L)
+{
+	double n1 = lua_tonumber(L, 1);	
+	double n2 = lua_tonumber(L, 2);	
+
+	lua_pushnumber(L, n1+n2);
+
+	return 1;
+}
---
+++ api/test.lua
@@ -0,0 +1,2 @@
+-- The exported function has different name from internal C function
+x = add(20, 7)
